667ca4c3c303b10a739286560c2cf8eef6de7486
dunice <dunice@Mac-mini-dunice.local>
2024-06-28 16:34:28 +0300

    First commit. New files: index.html, style.css, script.js. Make: head string, input, btn, styles for head string and input

---
+++ scripts/script.js
@@ -0,0 +1,40 @@
+// let mass;
+// mass ='gdgdg';
+
+// console.log(mass);
+
+// mass = 99;
+// console.log(mass);
+
+// const mas = 'hello';
+// console.log(mas)
+
+// let number1;
+// number1 = 2;
+// let number2;
+// number2 = number1;
+
+// console.log(number1 + number2);
+
+
+// let name = 'Tom';
+// let admin;
+// admin = name;
+// console.log(admin);
+
+// let numb1 = 12;
+// let numb2 = 14;
+// console.log(`${numb1 < numb2}`)
+
+
+// result = prompt('title', 100);
+// quest = confirm('???');
+// console.log(result);
+
+
+// compl = prompt('Сколько вам лет?', 0);
+// yn = confirm('Ваш возраст: ' + compl);
+// console.log(`${yn + compl}`);
+
+// nameUser = prompt('Ваше имя')
+
